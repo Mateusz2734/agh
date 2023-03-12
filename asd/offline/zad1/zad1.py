6f4e69be53d7b8817e6267ced9d1233272acd7ca
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-03-12 20:34:21 +0100

    Add solution to first offline exercise

---
+++ asd/offline/zad1/zad1.py
@@ -0,0 +1,19 @@
+from zad1testy import runtests
+
+def ceasar(s):
+    n = len(s)
+    longest_pal = 1
+    for i in range(1,n-1):
+        k = 1
+        while i - k > 0 and i + k < n:
+            if s[i - k] == s[i + k]:
+                if longest_pal < 2*k+1:
+                    longest_pal = 2*k + 1
+                k += 1
+            else:
+                break
+    return longest_pal
+
+# print(ceasar())
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests(ceasar , all_tests = True)
