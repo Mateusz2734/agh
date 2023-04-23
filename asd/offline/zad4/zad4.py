937de9c970322a9f3c6112e3aecd2b53a058f7ff
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-04-23 19:50:33 +0200

    Add full solution

--- asd/offline/zad4/zad4.py
+++ asd/offline/zad4/zad4.py
@@ -1,5 +1,11 @@
+# Mateusz Wala
+# Zamysł algorytmu: za pomocą BFS znaujdujemy najkrótszą (w sensie ilości krawędzi) ścieżkę pomiędzy wierzchołkami s i t.
+# Następnie dla każdej krawędzi na tej ścieżce sprawdzamy czy istnieje ścieżka pomiędzy s i t, która nie przechodzi
+# przez tę krawędź oraz czy jest ona dłuższa od najkrótszej znalezionej ścieżki.
+# Zwracamy krawędź, która stworzy najdłuższą ścieżkę pomiędzy s i t.
 from collections import deque
 from zad4testy import runtests
+
 
 def bfs(G, s):
     n = len(G)
@@ -19,6 +25,7 @@ def bfs(G, s):
                 Q.append(v)
     return (parent, visited)
 
+
 def bfs_ignore_edge(G, s, ignore):
     n = len(G)
     visited = [False for _ in range(n)]
@@ -31,11 +38,12 @@ def bfs_ignore_edge(G, s, ignore):
     while Q:
         u = Q.popleft()
         for v in G[u]:
-            if not visited[v] and (u,v) != ignore and (v,u) != ignore:
+            if not visited[v] and (u, v) != ignore and (v, u) != ignore:
                 visited[v] = True
                 parent[v] = u
                 Q.append(v)
     return (parent, visited)
+
 
 def calc_path(parent, s, t):
     path = []
@@ -44,24 +52,27 @@ def calc_path(parent, s, t):
         t = parent[t]
     return path
 
-def longer( G, s, t ):
+
+def longer(G, s, t):
     par, vis = bfs(G, s)
     if not vis[t]:
         return None
-    
-    # It will hold the path from t to s
+
     path = calc_path(par, s, t)
-    length = len(path) - 1
-    edges = [(path[i], path[i+1]) for i in range(len(path)-1)]
+    max_length = len(path) - 1
+    to_remove = None
+    edges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
 
     for edge in edges:
         par, vis = bfs_ignore_edge(G, s, edge)
         if not vis[t]:
             return edge
-        if len(calc_path(par, s, t)) > length:
-            return edge
+        curr_length = len(calc_path(par, s, t)) - 1
+        if curr_length > max_length:
+            max_length = curr_length
+            to_remove = edge
+    return to_remove
 
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( longer, all_tests = True )
-
+runtests(longer, all_tests=True)
