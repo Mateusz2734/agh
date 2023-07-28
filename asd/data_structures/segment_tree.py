505ec445e3471b236193e59134234e85bded1bd1
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-07-28 18:41:32 +0200

    fix: Add time complexity and more operations

--- asd/data_structures/segment_tree.py
+++ asd/data_structures/segment_tree.py
@@ -1,10 +1,12 @@
 class SegmentTree:
-    def __init__(self, T):
+    def __init__(self, T, operation=lambda a, b: a + b):
         self.n = len(T)
         self.tree = [0 for _ in range(2 * self.n)]
+        self.operation = operation
 
         self.build_tree(T)
 
+    # O(n)
     def build_tree(self, T):
         n = self.n
 
@@ -12,8 +14,9 @@ class SegmentTree:
             self.tree[n + i] = T[i]
 
         for i in range(n - 1, 0, -1):
-            self.tree[i] = self.tree[i << 1] + self.tree[i << 1 | 1]
+            self.tree[i] = self.operation(self.tree[i << 1], self.tree[i << 1 | 1])
 
+    # O(logn)
     def update(self, i, x):
         n = self.n
 
@@ -21,11 +24,12 @@ class SegmentTree:
         i += n
 
         while i > 1:
-            self.tree[i >> 1] = self.tree[i] + self.tree[i ^ 1]
+            self.tree[i >> 1] = self.operation(self.tree[i], self.tree[i ^ 1])
             i >>= 1
 
-    # [l, r)
-    def sum(self, l, r):
+    # [l, r) 
+    # O(logn)
+    def query(self, l, r):
         res = 0
 
         l += self.n
@@ -33,12 +37,12 @@ class SegmentTree:
 
         while l < r:
             if l & 1:
-                res += self.tree[l]
+                res = self.operation(res, self.tree[l])
                 l += 1
 
             if r & 1:
                 r -= 1
-                res += self.tree[r]
+                res = self.operation(res, self.tree[r])
 
             l >>= 1
             r >>= 1
