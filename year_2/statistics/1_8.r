f6bccf9f5d05e4378687c2209847f911dcbea758
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-10-12 17:29:17 +0200

    fix: Change the task

--- year_2/statistics/1_8.r
+++ year_2/statistics/1_8.r
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 # Napisz funkcję obliczającą odchylenie standardowe w próbie (zadanej jako wektor v).
 # Porównaj działanie z funkcją biblioteczną sd.
 # Czy można wykorzystać tę funkcję do obliczenia odchylenia standardowego dla całej populacji?
-=======
-# Napisz funkcję obliczającą wariancję w próbie (zadanej jako wektor v).
-# Porównaj działanie z funkcją biblioteczną var.
-# Czy można wykorzystać tę funkcję do obliczenia wariancji dla całej populacji?
->>>>>>> 3c63e9c878024ee4907394fcbb8c07c04c20d03a
 
 std <- function(v) {
   return(sqrt(var(v)))
