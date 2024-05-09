d84739ba0445fd95ea876a4813049a92c29dbc1d
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-09 21:28:48 +0200

    Add initial elevator script

---
+++ year_2/digital_technology/elevator.py
@@ -0,0 +1,38 @@
+from tabulate import tabulate
+import pandas as pd
+
+states = ["GO", "GC", "1O", "1C", "2O", "2C"]
+states_bin = {"GO": "1100", "GC": "0100", "1O": "1010", "1C": "0010", "2O": "1001", "2C": "0001"}
+
+df = pd.read_csv("data.csv")
+df["CURR_BIN"] = df["CURR"].map(states_bin)
+df["NEXT_BIN"] = df["NEXT"].map(states_bin)
+df["INPUTS (G12)"] = df["INPUTS (G12)"].astype(str).str.zfill(3)
+
+df=df[["CURR", "CURR_BIN", "INPUTS (G12)", "NEXT", "NEXT_BIN"]]
+
+print(tabulate(df, headers="keys", tablefmt="grid", showindex=False, colalign=("center", "center", "center", "center", "center")))
+
+k=0
+lines = []
+for k in range(4):
+    zeros = []
+    ones = []
+    for _, row in df.iterrows():
+        i = int(str(row["CURR_BIN"]), 2)
+        j = int(str(row["INPUTS (G12)"]), 2)
+        if row["NEXT_BIN"][k] == "1":
+            ones.append(j*16+i)
+        else:
+            zeros.append(j*16+i)
+    dontcares = [x for x in range(16*8) if x not in zeros and x not in ones]
+
+    lines.append((k, ",".join([str(elem) for elem in ones]), ",".join([str(elem) for elem in dontcares]), "\n"))
+
+with open("output.txt", "w") as f:
+    for line in lines:
+        f.write(f"K={line[0]}\n")
+        f.write(f"1s: {line[1]}\n")
+        f.write(f"DCs: {line[2]}\n")
+        f.write("\n")
+    f.write("\n")
