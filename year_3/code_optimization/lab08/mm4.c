d8a8dd3d92a62cb8e4ad13ab4947ae343ebb83e2
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-06-03 23:30:58 +0200

    Add lab08

---
+++ year_3/code_optimization/lab08/mm4.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+static double gtod_ref_time_sec = 0.0;
+#define BLOCK_SIZE 16
+#define PREFETCH_DISTANCE 64
+
+/* Adapted from the bl2_clock() routine in the BLIS library */
+
+double dclock()
+{
+  double the_time, norm_sec;
+  struct timeval tv;
+  gettimeofday( &tv, NULL );
+  if ( gtod_ref_time_sec == 0.0 )
+    gtod_ref_time_sec = ( double ) tv.tv_sec;
+  norm_sec = ( double ) tv.tv_sec - gtod_ref_time_sec;
+  the_time = norm_sec + tv.tv_usec * 1.0e-6;
+  return the_time;
+}
+
+
+//subroutine for optimization
+int mm(double ** first, double ** second, double ** multiply, int SIZE)
+{
+  int i,j,k,ii,jj,kk; 
+  double sum = 0;
+  double local_first[BLOCK_SIZE * BLOCK_SIZE];
+
+  for (ii = 0; ii < SIZE; ii += BLOCK_SIZE) {
+    for (jj = 0; jj < SIZE; jj += BLOCK_SIZE) {
+      for (kk = 0; kk < SIZE; kk += BLOCK_SIZE) {
+        for (i = 0; i < BLOCK_SIZE && (ii + i) < SIZE; i++) {
+          for (j = 0; j < BLOCK_SIZE && (kk + j) < SIZE; j++) {
+            local_first[i * BLOCK_SIZE + j] = first[ii + i][kk + j];
+          }
+        }
+        for (i = ii; i < ii + BLOCK_SIZE && i < SIZE; i++) {
+          for (j = jj; j < jj + BLOCK_SIZE && j < SIZE; j++) {
+            for (k = kk; k < kk + BLOCK_SIZE && k < SIZE; k++) {
+              sum += local_first[(i - ii) * BLOCK_SIZE + (k - kk)] * second[k][j];
+            }
+            multiply[i][j] += sum;
+            sum = 0;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+
+
+int main( int argc, const char* argv[] ){
+  int i,j,iret;
+  double ** first;
+  double ** second;
+  double ** multiply;
+
+  double * first_;
+  double * second_;
+  double * multiply_;
+  
+  double dtime;
+
+  int SIZE = atoi(argv[1]);
+
+//allocate blocks of continous memory
+  first_ = (double*) malloc(SIZE*SIZE*sizeof(double));
+  second_ = (double*) malloc(SIZE*SIZE*sizeof(double));
+  multiply_ = (double*) malloc(SIZE*SIZE*sizeof(double));
+
+//allocate 2D matrices
+  first = (double**) malloc(SIZE*sizeof(double*));
+  second = (double**) malloc(SIZE*sizeof(double*));
+  multiply = (double**) malloc(SIZE*sizeof(double*));
+
+//set pointers to continous blocks
+  for (i = 0; i < SIZE; i++) {
+    first[i] = first_ + i*SIZE;
+    second[i] = second_ + i*SIZE;
+    multiply[i] = multiply_ + i*SIZE;
+  }
+
+//fill matrices with test data
+  for (i = 0; i < SIZE; i++) { //rows in first
+    for (j = 0; j < SIZE; j++) { //columns in first
+      first[i][j]=i+j;
+      second[i][j]=i-j;
+      multiply[i][j]=0;
+    }
+  }
+
+//measure mm subroutine computation time
+  dtime = dclock();
+  iret = mm(first,second,multiply,SIZE); 
+  dtime = dclock()-dtime;
+  printf( "Time: %le \n", dtime);
+
+    double sum = 0;
+  for (i = 0; i < SIZE; i++) { //rows in multiply
+    for (j = 0; j < SIZE; j++) { //columns in multiply
+      sum += multiply[i][j];
+    }
+  }
+  printf( "Sum of all elements in multiply: %le \n", sum);
+
+  fflush( stdout );
+
+//cleanup
+  free(first_);
+  free(second_);
+  free(multiply_);
+
+  free(first);
+  free(second);
+  free(multiply);
+
+  return iret;
+}
+ 
